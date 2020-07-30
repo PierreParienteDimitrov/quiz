ca0453838f52a15d5777dddf18d5912501c7d888
Pierre Pariente <pierre.parientedimitrov@gmail.com>
2020-07-30 09:48:35 -0700

    Created overal structure and logic

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,103 @@
-// DOM ELEMENTS
-startQuiz = document.querySelector('#start-quiz')
-questionsDiv = document.querySelector('#questions-div')
-hideDiv = document.querySelector('#hide-div')
+// DOM elements
+var startQuiz = document.querySelector('#start-quiz')
+var questionsDiv = document.querySelector('#questions-div')
+var introDiv = document.querySelector('.intro-div')
 
-var secondsLeft = 76
+// global variables
+
+var questions = [
+  {
+    question: "Commonly used data types DO NOT include:",
+    answers: ["Strings", "Booleans", "Alerts", "Numbers"],
+    correctAnswer: "Alerts",
+  },
+  {
+    question: "The condition in an if / else statement is enclosed within:",
+    answers: ["Quotes", "Curly brackets", "Parentheses", "Square brackets"],
+    correctAnswer: "Parentheses",
+  },
+  {
+    question: "Arrays in JavaScript can be used to store:",
+    answers: ["Numbers and strings", "Other arrays", "Booleans", "All of the above"],
+    correctAnswer: "All of the above",
+  },
+  {
+    question: "String values must be enclosed within___ when being assigned to variables",
+    answers: ["Commas", "Curly brackets", "Quotes", "Parenthesis"],
+    correctAnswer: "Quotes",
+  },
+  {
+    question: "A very useful too used during development and debugging for printing content to the debugger is:",
+    answers: ["JavaScript", "Terminal/Bash", "For loops", "console.log"],
+    correctAnswer: "console.logs",
+  },
+];
+
+var secondsLeft = 10
+
+// functions
+
+function startGame() {
+    introDiv.classList.add('hide-div')
+    
+    startTimer()
+    displayFunction()
+
+}
+
+function displayFunction() {
+    var currentQuestion = document.createElement('h3')
+    currentQuestion.textContent = questions[0].question
+    questionsDiv.appendChild(currentQuestion)
+
+
+    // display four buttons
+
+    for ( i = 0 ; i < questions[0].answers.length ; i++) {
+        var buttons = document.createElement('button')
+        buttons.textContent = questions[0].answers[i]
+        questionsDiv.appendChild(buttons)
+        buttons.classList.add('btn-display')
+    }
+
+
+
+}
+
+function endGame(){
+    console.log('end game')
+
+}
+
+function startTimer() {
+    var timerInterval = setInterval(function(){
+        secondsLeft --
+        var countDown = document.querySelector('#countdown')
+        countDown.textContent = secondsLeft
+
+        if(secondsLeft === 0) {
+            clearInterval(timerInterval)
+            endGame()
+        }
+    }, 1000)
+}
+
+// function that monitors the click on the answer and displays the next question
+
+// on click events
+
+startQuiz.addEventListener('click', startGame)
+
+
+
+// when bitton start is clicked, call a function that will display the questions
+
+// DOM ELEMENTS
+// startQuiz = document.querySelector('#start-quiz')
+// questionsDiv = document.querySelector('#questions-div')
+// hideDiv = document.querySelector('#hide-div')
+
+// var secondsLeft = 76
 
 // var myQuestions = [
 //     {
@@ -18,45 +112,44 @@ var secondsLeft = 76
 //     }
 // ]
 
-var questionArray = ["this is a question"]
+// var questionArray = ["this is a question"]
 
-// BUTTON START QUIZ
-startQuiz.addEventListener("click", function(event) {
-    hideDiv.style.display = 'none'
-    question1()
-    timer()
-})
+// // BUTTON START QUIZ
+// startQuiz.addEventListener("click", function(event) {
+//     hideDiv.style.display = 'none'
+//     question1()
+//     timer()
+// })
 
 // QUIZ QUESTIONS
-function question1() {
-    var h3Tag = document.createElement('h3')
-    var buttonArray = ['1. Strings', '2. booleans', '3. alerts', '4. numbers']
+// function question1() {
+//     var h3Tag = document.createElement('h3')
+//     var buttonArray = ['1. Strings', '2. booleans', '3. alerts', '4. numbers']
 
-    h3Tag.textContent = questionArray[0]
-    questionsDiv.appendChild(h3Tag)
+//     h3Tag.textContent = questionArray[0]
+//     questionsDiv.appendChild(h3Tag)
 
-    for(let i = 0; i < buttonArray.length ; i++){
-        var button = button + [i]
-        button = document.createElement('button')
-        button.textContent = buttonArray[i]
-        questionsDiv.appendChild(button)
-        button.setAttribute("style", "display: block;")
-        button.classList.add('.button-answer')
-    }
+//     for(let i = 0; i < buttonArray.length ; i++){
+//         var button = button + [i]
+//         button = document.createElement('button')
+//         button.textContent = buttonArray[i]
+//         questionsDiv.appendChild(button)
+//         button.setAttribute("style", "display: block;")
+//         button.classList.add('.button-answer')
+//     }
 
-    // document.querySelector('button-answer').addEventListener("click")
+//     // document.querySelector('button-answer').addEventListener("click")
 
-
-}
+// }
 
 // TIMER FUNCTION
-function timer() {
-    var timerInterval = setInterval(function(){
-        secondsLeft --
-        document.querySelector('#countdown').textContent = secondsLeft
+// function timer() {
+//     var timerInterval = setInterval(function(){
+//         secondsLeft --
+//         document.querySelector('#countdown').textContent = secondsLeft
 
-        if(secondsLeft === 0) {
-            clearInterval(timerInterval)
-        }
-    }, 1000)
-}
+//         if(secondsLeft === 0) {
+//             clearInterval(timerInterval)
+//         }
+//     }, 1000)
+// }
